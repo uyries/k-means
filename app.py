b4412d47d93dd6b4f375a6268ca8b160056a27d7
jay <167955116+uyries@users.noreply.github.com>
2025-04-20 16:47:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 20 16:14:15 2025
-
-@author: LAB
-"""
-
 import streamlit as st
-import pickle
 import matplotlib.pyplot as plt
+import pickle
 from sklearn.datasets import make_blobs
 
-#load model
+# Load model
 with open('kmeans_model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
-    
-#set the title
-st.tilte("K-means Clustering Visualization by Pimnara")
+   loaded_model = pickle.load(f)
 
-#set the page config
-st.set_page_config(page_title = "K-means Clustering", layout = "centered")
+st.set_page_config(page_title="k-means Clustering ", layout="centered")
 
-#load dataset
+st.title("k-means Clustering Visualizer by Pimnara")
+
+# Generate example data
 X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
 
-#Predict using the loaded model
+# Predict cluster labels using the loaded k-means model
 y_kmeans = loaded_model.predict(X)
 
-#plotting
+# Create scatter plot
 fig, ax = plt.subplots()
-scatter =  ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+scatter= ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
 ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
 ax.set_title('k-Means Clustering')
 ax.legend()
